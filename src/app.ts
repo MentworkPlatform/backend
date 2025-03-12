a00c8799b759343b2835a5e91ed3916473b605ac
aheedjo <maishanu.ahijo@yahoo.com>
2025-03-12 02:24:48 +0100

    Add database configuration and connection

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import pool from './db';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+dotenv.config();
 app.use(express.json());
+
+pool.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Database connection error', err.stack));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Mentwork!');
